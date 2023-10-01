f0e94921841003ec637787dfbcb16268c8b90bc4
rolandotrullotododesmadre <james.disak@gmail.com>
2023-09-30 20:50:47 -0400

    arreglando error de borrado ded formato de img de dashboard style.css

--- php/ventas.php
+++ php/ventas.php
@@ -7,7 +7,7 @@
 	<!-- Boxicons -->
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<!-- My CSS -->
-	<link rel="stylesheet" href="../assets/css/cssDashBoard/style.css">
+	<link rel="stylesheet" href="../assets/css/cssDashBoard/styleventas.css">
 	<link rel="stylesheet" href="../assets/css/cssDashBoard/mods.css">
 	<link rel="stylesheet" href="../assets/css/cssDashBoard/ventas.css">
 	<title>AdminHub</title>
