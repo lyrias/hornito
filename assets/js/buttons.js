4d20444a0f4d15c6fcaffd0785c00ecb322b9917
rolandotrullotododesmadre <james.disak@gmail.com>
2023-09-30 22:17:48 -0400

    arreglando error de borrado ded formato de img de dashboard style.css

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -5,44 +5,51 @@ function aumentarSC(){ // se crean la funcion y se agrega al evento onclick en e
     var x = document.getElementById('cantidadSC').value = ++inicio; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
     var z = parseInt(x)*5
     console.log(z)
-    document.getElementById("total").value = z
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 function disminuirSC(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
     console.log( "Ejecutando función test() disminuir" );
     var x = document.getElementById('cantidadSC').value = --inicio; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
-
+    var z = parseInt(x)*5
+    console.log(z)
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 
 var inicio1 = 1; //se inicializa una variable en 0
-var precio1 = 5;
 function aumentarSP(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
     console.log( "Ejecutando función test() aumentar" );
     var x = document.getElementById('cantidadSP').value = ++inicio1; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+    var z = parseInt(x)*5
+    console.log(z)
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 function disminuirSP(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
     console.log( "Ejecutando función test() disminuir" );
     var x = document.getElementById('cantidadSP').value = --inicio1; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    var z = parseInt(x)*5
+    console.log(z)
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 
 var inicio2 = 1; //se inicializa una variable en 0
-var precio2 = 2;
 function aumentarMS(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
     console.log( "Ejecutando función test() aumentar" );
     var x = document.getElementById('cantidadMS').value = ++inicio2; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+    var z = parseInt(x)*2
+    console.log(z)
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 function disminuirMS(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
     console.log( "Ejecutando función test() disminuir" );
     var x = document.getElementById('cantidadMS').value = --inicio2; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    var z = parseInt(x)*2
+    console.log(z)
+    document.getElementById("total").value = parseInt(document.getElementById("total").value)+z
 }
 
 document.oncontextmenu = function(){return false}
-
-function total(){
-    var total= document.getElementById('total').value = 0;
-    suma = 0
-}
