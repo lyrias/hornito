e9e4e041db0c508efbf43019e11966c2a743376c
rolandotrullotododesmadre <james.disak@gmail.com>
2023-11-13 22:14:08 -0400

    se modifico el javascript para centralizar todos los cambios en uno

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -2,21 +2,25 @@ var inicio = 0; //se inicializa una variable en 0
 var precio = 5;
 function aumentar(n, cat){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
     console.log( "Ejecutando función test() aumentar" );
-    var x = document.getElementById('cantidad'+cat).value = parseInt(document.getElementById('cantidad'+cat).value)+n; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+    console.log(n);
+    console.log(typeof n);
+    console.log(cat);
+    console.log(typeof cat);
+    var x = document.getElementById('cantidad'+cat.toString()).value = parseInt(document.getElementById('cantidad'+cat.toString()).value)+n; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
     console.log(x);
-    var z = document.getElementById('precio'+cat).value;
+    var z = parseInt(document.getElementById('precio'+cat.toString()).value);
     console.log(z);
-    var g = document.getElementById("total"+cat).value = parseInt(document.getElementById("cantidad"+cat).value)*z;
+    var g = document.getElementById("total"+cat.toString()).value = parseInt(document.getElementById("cantidad"+cat.toString()).value)*z;
     console.log(g);
 }
 
 function disminuir(n, cat){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
     console.log( "Ejecutando función test() disminuir" );
-    var x = document.getElementById('cantidad'+cat).value = parseInt(document.getElementById('cantidad'+cat).value)-n; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    var x = document.getElementById('cantidad'+cat.toString()).value = parseInt(document.getElementById('cantidad'+cat.toString()).value)-n; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
     console.log(x);
-    var z = document.getElementById('precio'+cat).value;
+    var z = parseInt(document.getElementById('precio'+cat.toString()).value);
     console.log(z);
-    var g=document.getElementById("total"+cat).value = parseInt(document.getElementById("cantidad"+cat).value)*z;
+    var g=document.getElementById("total"+cat.toString()).value = parseInt(document.getElementById("cantidad"+cat.toString()).value)*z;
     console.log(g);
 }
 
